aa9f0a90af0da1eef9ce98255341b6964b1fe3ed
alfredo-petri <146951566+alfredo-petri@users.noreply.github.com>
2023-12-27 16:55:50 -0300

    form

--- projeto-portifolio-ux-ui-desginer/assets/script.js
+++ projeto-portifolio-ux-ui-desginer/assets/script.js
@@ -3,7 +3,13 @@
 //variável para selecionar botão
 let btnContact = document.querySelector('.jl-btn-contact');
 
+//variável para selecionar preloader
 let pagePreloader = document.querySelector('.jl-preloader');
+
+//variavel para selecionar botão de orçamento
+
+let toggleModal = document.querySelectorAll('.jl-toggle-modal')
+
 
 //função page preloader
 window.addEventListener('load', function () {
@@ -20,6 +26,20 @@ window.addEventListener('load', function () {
 //função toogle on/off contact info
 btnContact.addEventListener('click', function () {
     let boxContact = document.querySelector('.jl-contact-info');
-    boxContact.classList.toggle('jl-btn-contact-info-is-on');
+    boxContact.classList.toggle('jl-is-on');
     this.classList.toggle('jl-change-icon');
 });
+
+//abrindo e fechando modal de orçamento
+for (let i = 0; i < toggleModal.length; i++){
+    toggleModal[i].addEventListener('click', function(){
+        let overlay = document.querySelector('.jl-overlay');
+        let modalOrcamento = document.querySelector('#jl-modal-orcamento');
+
+        overlay.classList.toggle('jl-is-on');
+        modalOrcamento.classList.toggle('jl-is-on');
+        setTimeout(function(){
+            modalOrcamento.classList.toggle('jl-slide-in');
+        }, 300);
+    })
+}
