d429e83e9e4bd992ed2a2e2a3d7bd862bed32920
alfredo-petri <146951566+alfredo-petri@users.noreply.github.com>
2023-12-29 10:51:02 -0300

    slider portifólio documentação

--- projeto-portifolio-ux-ui-desginer/assets/script.js
+++ projeto-portifolio-ux-ui-desginer/assets/script.js
@@ -1,22 +1,10 @@
-//declarando variáveis
+//INICIO PRELOADER
 
-//variável para selecionar botão
-let btnContact = document.querySelector('.jl-btn-contact');
-
-//variável para selecionar preloader
+// declarando variável para selecionar preloader
 let pagePreloader = document.querySelector('.jl-preloader');
 
-//variavel para selecionar botão de orçamento
-let toggleModal = document.querySelectorAll('.jl-toggle-modal')
 
-//variavel para receber btn acroll down
-let scrollDown = document.querySelector('.jl-scroll-down')
-
-
-
-//funções
-
-//função page preloader
+// função page preloader
 window.addEventListener('load', function () {
 
     setTimeout(function(){
@@ -28,14 +16,36 @@ window.addEventListener('load', function () {
     }, 3000);
 });
 
-//função toogle on/off contact info
+//FIM PRELOADER 
+
+
+
+
+//INICIO CONTACT INFO
+
+// variável para selecionar botão contact
+let btnContact = document.querySelector('.jl-btn-contact');
+
+// função toogle on/off contact info
 btnContact.addEventListener('click', function () {
     let boxContact = document.querySelector('.jl-contact-info');
     boxContact.classList.toggle('jl-is-on');
     this.classList.toggle('jl-change-icon');
 });
 
-//abrindo e fechando modal de orçamento
+
+//FIM CONTACT INFO
+
+
+
+
+
+//INICIO MODAL ORÇAMENTO
+
+// variavel para selecionar botão de orçamento
+let toggleModal = document.querySelectorAll('.jl-toggle-modal')
+
+// abrindo e fechando modal de orçamento
 for (let i = 0; i < toggleModal.length; i++){
     toggleModal[i].addEventListener('click', function(){
         let overlay = document.querySelector('.jl-overlay');
@@ -49,40 +59,70 @@ for (let i = 0; i < toggleModal.length; i++){
     })
 }
 
-//animando elementos on scroll com waypoints
+
+//FIM MODAL ORÇAMENTO
+
+
+
+
+// variavel para receber btn acroll down
+let scrollDown = document.querySelector('.jl-scroll-down')
+
+// animando elementos on scroll com waypoints
 var waypoint = new Waypoint({
     element: scrollDown,
     handler: function() {
         scrollDown.classList.toggle('jl-fade-out'); 
     }, offset: '75%'
-  }) 
+}) 
+
+//FIM FADEOUT SCROLL DOWN ANIMATION  
 
 
-  // PORTIFÓLIO SLIDER
 
-  // Declarando váriaveis slider
 
-  let sliderContainer = document.querySelector ('.jl-slider-container');
-  let sliderList = document.querySelector ('.jl-slider-list');
-  let sliderItem = document.querySelectorAll ('.jl-slider-item');
-  let sliderListWidth = null;
+// INICIO PORTIFÓLIO SLIDER
 
-  // Declarando variáveis de captura de larguras individuais
 
-  let containerWidth = sliderContainer.parentElement.offsetWidth;
-  
+// Declarando váriaveis slider
 
-  // Passando larguras dinâmicas
+// variavel que recebe o elemento slider container
+let sliderContainer = document.querySelector ('.jl-slider-container');
+// variavel que recebe o elemento slider list
+let sliderList = document.querySelector ('.jl-slider-list');
+// variavel que recebe o elemento slider item
+let sliderItem = document.querySelectorAll ('.jl-slider-item');
 
-  sliderContainer.style.width = containerWidth+'px';
 
-  for ( let indexSliderItem = 0; indexSliderItem < sliderItem.length; indexSliderItem ++){
-    sliderItem[indexSliderItem].style.width = containerWidth+'px'; 
+// Declarando variáveis de captura de larguras individuais
 
-    let sliderItemWidth = sliderItem[indexSliderItem].offsetWidth;
-    
-    sliderListWidth +=  sliderItemWidth;
-  }
+// variavel que atribui uma largura dinâmica para o container 
+let containerWidth = sliderContainer.parentElement.offsetWidth;
+// variavel que irá atribuir uma largura dinâmica para o slider list
+let sliderListWidth = null;
 
-  sliderList.style.width = sliderListWidth + 'px';
-  // Animação Slider onClick
+
+// Passando larguras dinâmicas
+
+// atribui largura dinamica, capturada, ao elemento slider container
+sliderContainer.style.width = containerWidth + 'px';
+
+// loop para atribuir largura dinamica ao elemento slider item
+for ( let indexSliderItem = 0; indexSliderItem < sliderItem.length; indexSliderItem ++){
+sliderItem[indexSliderItem].style.width = containerWidth+'px'; 
+
+// variavel que captura a largura dinamica de cada item 
+let sliderItemWidth = sliderItem[indexSliderItem].offsetWidth;
+
+//definindo a largura total do elemento slider list dinamicamente
+sliderListWidth +=  sliderItemWidth;
+}
+
+// atribuindo a largura total do elemento slider list
+sliderList.style.width = sliderListWidth + 'px';
+
+
+// Animação Slider onClick
+
+
+// FIM PORTIFOLIO SLIDER
