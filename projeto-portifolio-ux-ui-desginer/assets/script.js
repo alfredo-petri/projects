7b74b2622616256a95d0c1ad223edbe4e8f958e0
alfredo-petri <146951566+alfredo-petri@users.noreply.github.com>
2023-12-29 11:16:59 -0300

    update slider portifólio documentação

--- projeto-portifolio-ux-ui-desginer/assets/script.js
+++ projeto-portifolio-ux-ui-desginer/assets/script.js
@@ -5,12 +5,16 @@ let pagePreloader = document.querySelector('.jl-preloader');
 
 
 // função page preloader
+
+// função acionada quando a página está totalmente carregada 
 window.addEventListener('load', function () {
 
+// adiciona a classe css .jl-fade-out ao elemento jl-page-preloader, com timer    
     setTimeout(function(){
         pagePreloader.classList.add ('jl-fade-out'); 
     }, 750);
-    
+
+// adiciona style css display none ao elemento jl-page-preloader, com timer       
     setTimeout(function(){
         pagePreloader.style.display = 'none';
     }, 3000);
@@ -27,9 +31,16 @@ window.addEventListener('load', function () {
 let btnContact = document.querySelector('.jl-btn-contact');
 
 // função toogle on/off contact info
-btnContact.addEventListener('click', function () {
+
+// função acionada através de click no botão
+btnContact.addEventListener('click', function () { 
+
+//variavel para receber a caixa de informação
     let boxContact = document.querySelector('.jl-contact-info');
+
+// adiciona a classe css .jl-is-on ao elemento jl-contact-info
     boxContact.classList.toggle('jl-is-on');
+// adiciona a classe css .jl-is-on ao elemento jl-btn-contact
     this.classList.toggle('jl-change-icon');
 });
 
@@ -42,17 +53,27 @@ btnContact.addEventListener('click', function () {
 
 //INICIO MODAL ORÇAMENTO
 
-// variavel para selecionar botão de orçamento
+// variavel para selecionar a classe css  .jl-toggle-modal presente no botão de orçamento
 let toggleModal = document.querySelectorAll('.jl-toggle-modal')
 
-// abrindo e fechando modal de orçamento
+// loop abrindo e fechando modal de orçamento
+// declaração de variavel i para iteração e estrutura do loop
 for (let i = 0; i < toggleModal.length; i++){
+
+// função acionada atravś de clique no botão de orçamento    
     toggleModal[i].addEventListener('click', function(){
+
+// declarando variavel para receber a classe css .jl-overlay        
         let overlay = document.querySelector('.jl-overlay');
+
+// declarando variavel para receber o ID css #jl-modal-orçamento presente no container do modal        
         let modalOrcamento = document.querySelector('#jl-modal-orcamento');
 
+// adiciona a classe css .jl-is-on ao elemento overlay
         overlay.classList.toggle('jl-is-on');
+// adiciona a classe css .jl-is-on ao elemento modal
         modalOrcamento.classList.toggle('jl-is-on');
+// timer para animation fade-in
         setTimeout(function(){
             modalOrcamento.classList.toggle('jl-slide-in');
         }, 300);
@@ -68,9 +89,13 @@ for (let i = 0; i < toggleModal.length; i++){
 // variavel para receber btn acroll down
 let scrollDown = document.querySelector('.jl-scroll-down')
 
-// animando elementos on scroll com waypoints
-var waypoint = new Waypoint({
+// animando elementos on scroll com a biblioteca waypoints
+
+// declarando novo objeto
+let waypoint = new Waypoint({
     element: scrollDown,
+
+// metodo que adiciona a animaçao fadeout ao elemento .jl-scroll-down quando rolar a página 25% para baixo 
     handler: function() {
         scrollDown.classList.toggle('jl-fade-out'); 
     }, offset: '75%'
