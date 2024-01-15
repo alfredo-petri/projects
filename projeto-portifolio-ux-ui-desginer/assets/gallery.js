6a53374ead74cb4d3b2bf9443ea24e7559dec670
alfredo-petri <146951566+alfredo-petri@users.noreply.github.com>
2024-01-15 19:50:40 -0300

    manage-it update

--- projeto-portifolio-ux-ui-desginer/assets/gallery.js
+++ projeto-portifolio-ux-ui-desginer/assets/gallery.js
@@ -3,6 +3,7 @@ let frameContainer = document.querySelector('.jl-gallery-frame-container');
 let frameImage = document.querySelector('.jl-gallery-frame-image');
 let galleryImages = document.querySelectorAll ('.jl-thumb-img');
 let imageSrc = null;
+let closeGallery = document.querySelectorAll('.jl-toggle-gallery')
 
 const getImageSrc =  function(){
     for (let i = 0; i < galleryImages.length; i++){
@@ -13,6 +14,13 @@ const getImageSrc =  function(){
             frameContainer.classList.add('jl-is-on')
         })
     }
+}
+
+for (let i = 0; i < closeGallery.length; i++){
+    closeGallery[i].addEventListener('click', function(){
+        overlay.classList.remove('jl-is-on')
+        frameContainer.classList.remove('jl-is-on')
+    })
 }
 
 getImageSrc();
