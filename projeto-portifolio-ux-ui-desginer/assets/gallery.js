c06edc317be20f59ab7d5c3a99459aea2e081cec
alfredo-petri <146951566+alfredo-petri@users.noreply.github.com>
2024-02-05 11:34:38 -0300

    responsive

--- projeto-portifolio-ux-ui-desginer/assets/gallery.js
+++ projeto-portifolio-ux-ui-desginer/assets/gallery.js
@@ -1,141 +1,141 @@
-var overlay = document.querySelector('.jl-overlay');
-var frameContainer = document.querySelector('.jl-gallery-frame-container');
-var frameImage = document.querySelector('.jl-gallery-frame-image');
-var galleryImages = document.querySelectorAll('.jl-thumb-img');
-var closeGallery = document.querySelectorAll('.jl-toggle-gallery');
-var btnNext = document.querySelector('.jl-item-next');
-var btnPrev = document.querySelector('.jl-item-prev');
-var currCounter = document.querySelector('.jl-current-slide');
-var totalCounter = document.querySelector('.jl-total-slide');
-var skeletonLoading = document.querySelector('.jl-skeleton-loading');
+// variavel de captura do elemento overlay
+let overlay = document.querySelector('.jl-overlay');
+//variavel de captura do container do frame
+let frameContainer = document.querySelector('.jl-gallery-frame-container');
+//variavel de captura da imagem do frame
+let frameImage = document.querySelector('.jl-gallery-frame-image');
+//variavel de captura da imagem da galeria qie se usada no frame
+let galleryImages = document.querySelectorAll ('.jl-thumb-img');
+//variavel de captura do caminho da imagem 
+let imageSrc = null;
+//variavel de captura de elementos responsaveis por fechar o frame
+let closeGallery = document.querySelectorAll('.jl-toggle-gallery');
+//variavel de captura do botao prev
+let btnPrev = document.querySelector ('.jl-item-prev');
+//variavel de captura do botao next
+let btnNext = document.querySelector ('.jl-item-next');
+//varprevl de captura do numero da imagem que será aplicada no frame
+let itemNum = null;
+//variavel que indica o numero do item atual no frame
+let currentItemNum = null;
+//variavel que indica o numero do proximo item no frame
+let nextItemNum = null;
+//variavel que ira receber o item atual
+let item = null;
+//variavel que ira receber o atributo data-item
+let dataItemNum = null;
+//variavel que captura o caminho da proxima imagem a ser exibida no frame
+let nextSrc = null;
+//variavel que captura o atributo data-index do proximo item
+let nextIndex = null;
 
-var postGallery = document.querySelector('.jl-post-gallery');
-var postGalleryHeight = postGallery.clientHeight;
-postGallery.style.height = (postGalleryHeight - 270) + 'px';
+let prevSrc = null;
+//variavel que captura o atributo data-index do proximo item
+let prevIndex = null;
 
-//Counter Formater
-var counterFormatter = function (n) {
-    if (n < 10) {
-        return '0' + n;
-    } else {
-        return n;
-    }
-}
+let skeletonLoading = document.querySelector ('.jl-skeleton-loading');
 
-totalCounter.innerHTML = counterFormatter(galleryImages.length);
-
-//Skeleton Loading
-const skeletonAnim = function (imagem) {
-    var myImage = new Image();
+const skeletonAnimation = function (imagem) {
+    const myImage = new Image();
     myImage.src = imagem;
-    myImage.addEventListener('load', function () {
+    myImage.addEventListener('load', function (){
         skeletonLoading.classList.add('jl-fade-out');
-        console.log('iniciou fadeOut');
         setTimeout(function () {
-            skeletonLoading.style.display = 'none';
-            console.log('iniciou display None');
+            skeletonLoading.style.display = 'none'
         }, 2000);
     });
 }
 
 
-//Open Gallery Modal
-const getImageSrc = function () {
-    for (var i = 0; i < galleryImages.length; i++) {
-        galleryImages[i].addEventListener('click', function () {
-            var imageSrc = this.getAttribute('data-src');
-            var itemNum = this.getAttribute('data-item');
+
+
+
+
+const totalCounterNumber = document.querySelector('.jl-total-slide').innerHTML = galleryImages.length.toString().padStart(2, '0');
+
+const applyCurrentNumber = (number) => {
+    document.querySelector('.jl-current-slide').innerHTML = number.toString().padStart(2, '0');
+} 
+
+//metodo para pegar a imagem e aplica-la no frame
+const getImageSrc =  function(){
+
+    //loop de captura das imagens na galeria
+    for (let i = 0; i < galleryImages.length; i++){
+        //função para pegar o caminho e num da imagem desejada e aplica-la ao frame, a partir de um click
+        galleryImages [i].addEventListener('click', function(){
+
+            imageSrc = this.getAttribute('data-src');
+            frameImage.setAttribute ('src', imageSrc);
 
             skeletonLoading.style.display = 'flex';
 
-            frameImage.setAttribute('src', imageSrc);
-            frameImage.setAttribute('data-index', itemNum);
-            overlay.classList.add('jl-is-open');
-            frameContainer.classList.add('jl-is-open');
-            currCounter.innerHTML = counterFormatter(itemNum);
+            itemNum = this.getAttribute('data-item');
+            frameImage.setAttribute ('data-index', itemNum);
+            
+            //adiciona visibilidade aos elementos overlay e frame de imagem
+            overlay.classList.add('jl-is-on');
+            frameContainer.classList.add('jl-is-on');
 
-            skeletonAnim(imageSrc);
-        });
+            skeletonAnimation(imageSrc);
+
+            applyCurrentNumber(itemNum);
+        })
     }
 }
+
 getImageSrc();
 
-for (var c = 0; c < closeGallery.length; c++) {
-    closeGallery[c].addEventListener('click', function () {
-        overlay.classList.remove('jl-is-open');
-        frameContainer.classList.remove('jl-is-open');
-    });
+//loop para fechar o frame
+for (let i = 0; i < closeGallery.length; i++){
+    closeGallery[i].addEventListener('click', function(){
+        //remove a visibilidade dos elementos overlay e frame de imagem
+        overlay.classList.remove('jl-is-on');
+        frameContainer.classList.remove('jl-is-on');
+    })
 }
 
-const nextItem = function () {
+const nextItem = function(){
 
-    //Identificamos o item atual no frame
-    var currItemNum = frameImage.getAttribute('data-index');
+    itemNum = parseInt(itemNum) +1;
+   
+    if (galleryImages[itemNum] == undefined) return;
 
-    //Definimos o numero do proximo item
-    var nextItemNum = parseInt(currItemNum) + 1;
+    item = galleryImages[itemNum];
+    nextSrc = item.getAttribute('data-src');
+    nextIndex = item.getAttribute('data-item');
 
+    skeletonLoading.style.display = 'flex';
 
-    //Fazemos o loop e identificamos qual item faz match com o numero proximo item 
-    for (var n = 0; n < galleryImages.length; n++) {
-        var item = galleryImages[n];
-        var itemNum = parseInt(item.getAttribute('data-item'));
-
-        if (itemNum === nextItemNum) {
-            //Capturamos o data-src
-            var nextSrc = item.getAttribute('data-src');
-            var nextIndex = item.getAttribute('data-item');
-
-            skeletonLoading.style.display = 'flex';
-
-            //Passamos o data-src para a tag de img no frame
-            frameImage.setAttribute('src', nextSrc);
-            frameImage.setAttribute('data-index', nextIndex);
-
-            currCounter.innerHTML = counterFormatter(nextIndex);
-
-            skeletonAnim(nextSrc);
-
-        }
-    }
+    frameImage.setAttribute('src', nextSrc);
+    frameImage.setAttribute('data-index', nextIndex); 
+    applyCurrentNumber(nextIndex);
+    skeletonAnimation(nextSrc);
 }
 
-const prevItem = function () {
+const prevItem = function(){
 
-    //Identificamos o item atual no frame
-    var currItemNum = frameImage.getAttribute('data-index');
+    itemNum = parseInt(itemNum) -1;
+   
+    if (galleryImages[itemNum] == undefined) return;
 
-    //Definimos o numero do proximo item
-    var prevItemNum = parseInt(currItemNum) - 1;
+    item = galleryImages[itemNum];
+    prevSrc = item.getAttribute('data-src');
+    prevIndex = item.getAttribute('data-item');
 
+    skeletonLoading.style.display = 'flex';
 
-    //Fazemos o loop e identificamos qual item faz match com o numero proximo item 
-    for (var p = 0; p < galleryImages.length; p++) {
-        var item = galleryImages[p];
-        var itemNum = parseInt(item.getAttribute('data-item'));
-
-        if (itemNum === prevItemNum) {
-            //Capturamos o data-src
-            var prevSrc = item.getAttribute('data-src');
-            var prevIndex = item.getAttribute('data-item');
-
-            skeletonLoading.style.display = 'flex';
-
-            //Passamos o data-src para a tag de img no frame
-            frameImage.setAttribute('src', prevSrc);
-            frameImage.setAttribute('data-index', prevIndex);
-
-            currCounter.innerHTML = counterFormatter(prevIndex);
-
-            skeletonAnim(prevSrc);
-        }
-    }
+    frameImage.setAttribute('src', prevSrc);
+    frameImage.setAttribute('data-index', prevIndex); 
+    applyCurrentNumber(prevIndex);
+    skeletonAnimation(prevSrc);
 }
 
-btnNext.addEventListener('click', function () {
-    nextItem();
-});
 
-btnPrev.addEventListener('click', function () {
-    prevItem();
-});
+btnNext.addEventListener('click', function(){
+    nextItem()
+})
+
+btnPrev.addEventListener('click', function(){
+    prevItem()
+})
