fe0591ed5d5812bcd88279bfe25dbe84ebde082d
alfredo-petri <146951566+alfredo-petri@users.noreply.github.com>
2024-02-08 21:51:20 -0300

    exercicio 1 js

---
+++ exercicios-javascript/exercicio-2.js
@@ -0,0 +1,6 @@
+// Declaração de variáveis para obtenção de dados via prompt
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Qual é a sua idade?");
+let cidade = prompt("Em que cidade você mora?");
+
+console.log ("O seu nome é " + nome + ", você tem " + idade + " anos e mora em " + cidade);
