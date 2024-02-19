7147fac1413c47fa54cdf80c37933af51c382511
alfredo_petri <alfredo_petri@hotmail.com>
2024-02-18 23:06:41 -0300

    calculadora

---
+++ aplicacoes-react/calculadora/src/components/Calculator.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import './Calculator.css';
+
+const Calculator = () => {
+
+    const [currentValue, setCurrentValue] = useState("0");
+    const [pendingOperation, setPendingOperation] = useState(null);
+    const [pendingValue, setPendingValue] = useState(null);
+    const [completeOperation, setCompleteOperation] = useState("");
+
+    const keyPadNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const operations = ['+', '-', '*', '/'];
+
+    const handleClick = (val) => {
+        setCurrentValue (prevValue => {
+            if (prevValue === '0') {
+                return val;
+            } else {
+                return prevValue + val;
+            }
+        });
+        setCompleteOperation (prevOperation => prevOperation + val);
+    };
+
+    const handlerClear = () => {
+        setCurrentValue ('0');
+        setPendingOperation (null);
+        setPendingValue (null);
+        setCompleteOperation ('');
+    };
+
+    return (
+
+        <div className="calculator">
+            
+            <div className="complete_operation">{completeOperation}</div>
+
+            <div className="display">{currentValue}</div>
+
+            <div className="buttons">
+                <button onClick={handlerClear}>AC</button>
+                
+                {keyPadNumbers.map((num) => (
+                    <button key={num} onClick={()=> handleClick (num)}>{num}</button>
+                ))}
+                
+                {operations.map((operation) => (
+                    <button key={operation}>{operation}</button>
+                ))}
+                
+                <button>=</button>
+            </div>
+
+            
+
+        </div>
+    )
+}
+
+export default Calculator
