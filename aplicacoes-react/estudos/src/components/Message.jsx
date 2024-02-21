f4a33b12e41b1220fd696a00166b9252af655f4e
alfredo_petri <alfredo_petri@hotmail.com>
2024-02-20 22:24:01 -0300

    update repositorio

--- aplicacoes-react/estudos/src/components/Message.jsx
+++
@@ -1,9 +0,0 @@
-const Message = ({msg}) => {
-  return (
-    <div>
-        <p>O nome do usuário é: {msg}.</p>
-    </div>
-  )
-}
-
-export default Message
--- aplicacoes-react/to-do-list/src/slices/todoSlice.js
+++
@@ -1,8 +0,0 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-
-export const todoSlice = createSlice ({
-    name: "todos",
-});
-
-export default todoSlice.reducer;
