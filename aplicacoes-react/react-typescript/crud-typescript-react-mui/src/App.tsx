86474f22edc46d3f2e3adc5a115e1046c6e63bc8
alfredo_petri <alfredo_petri@hotmail.com>
2024-03-22 00:24:15 -0300

    feat add react router dom

--- aplicacoes-react/react-typescript/crud-typescript-react-mui/src/App.tsx
+++ aplicacoes-react/react-typescript/crud-typescript-react-mui/src/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import { BrowserRouter } from "react-router-dom"
+import { AppRoutes } from "./routes"
 
 export const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   )
 }
