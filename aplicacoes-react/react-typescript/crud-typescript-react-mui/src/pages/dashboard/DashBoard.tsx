6cc1ef1f9046f3cf01ed73ad1474ed4531576742
alfredo_petri <alfredo_petri@hotmail.com>
2024-04-07 22:37:53 -0300

    create DashBoard

---
+++ aplicacoes-react/react-typescript/crud-typescript-react-mui/src/pages/dashboard/DashBoard.tsx
@@ -0,0 +1,10 @@
+import { LayoutBaseDePagina } from '../../shared/layouts/LayoutBaseDePagina'
+
+export const DashBoard = () => {
+  return (
+    <LayoutBaseDePagina titulo='titulo da pagina'>
+
+        testando mais um pouco
+    </LayoutBaseDePagina>
+  )
+}
