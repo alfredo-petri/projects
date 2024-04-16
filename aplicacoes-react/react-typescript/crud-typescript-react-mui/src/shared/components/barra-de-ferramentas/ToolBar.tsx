9e6d76e4b4f40c377cc65f53ad6246bbc803ccce
alfredo_petri <alfredo_petri@hotmail.com>
2024-04-16 10:35:14 -0300

    update design on Toolbar

--- aplicacoes-react/react-typescript/crud-typescript-react-mui/src/shared/components/barra-de-ferramentas/ToolBar.tsx
+++ aplicacoes-react/react-typescript/crud-typescript-react-mui/src/shared/components/barra-de-ferramentas/ToolBar.tsx
@@ -1,11 +1,32 @@
-import { Box, Button, TextField } from '@mui/material'
-import React from 'react'
+import {
+    Box,
+    Button,
+    Paper,
+    TextField,
+    useTheme,
+} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import AddIcon from '@mui/icons-material/Add';
+import React from "react";
 
 export const ToolBar: React.FC = () => {
-  return (
-    <Box>
-        <TextField />
-        <Button>Novo</Button>
-    </Box>
-  )
-}
+    const theme = useTheme();
+
+    return (
+        <Box
+            component={Paper}
+            sx={{
+                height: theme.spacing(5),
+                marginX: 1,
+                padding: 0.5,
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+                justifyContent: "space-between"
+            }}
+        >
+            <TextField size="small" placeholder="Pesquisar" InputProps={{endAdornment: <SearchIcon />}} />
+            <Button variant="contained" color="primary" disableElevation endIcon={<AddIcon />} sx={{color: "primary.contrastText"}}>Novo</Button>
+        </Box>
+    );
+};
