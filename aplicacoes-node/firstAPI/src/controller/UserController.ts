4528c52523c7f7cde60268a116bcd0a4ea771179
alfredo-petri <alfredo_petri@hotmail.com>
2024-07-03 07:23:41 -0300

    update feat createUser - UserController

--- aplicacoes-node/firstAPI/src/controller/UserController.ts
+++ aplicacoes-node/firstAPI/src/controller/UserController.ts
@@ -2,12 +2,11 @@ import { Request, Response } from "express"
 import { prisma } from "../database/prisma"
 
 
-export const createUser = async (request: Request, response: Response) => {
-    const { name, email, password } = request.body
+export const createUser = async (req: Request, res: Response) => {
+    const { name, email, password } = req.body
     const user = await prisma.user.create({
-        data: { name, email, password }
-        
+        data: { name, email, password },
     })
 
-    return response.json(user)
+    return res.json(user)
 }
