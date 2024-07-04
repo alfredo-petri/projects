7b658eae0827b82e2033d7c287407b6c2d765bf8
alfredo-petri <alfredo_petri@hotmail.com>
2024-07-04 12:54:43 -0300

    feat email validation on create user

--- aplicacoes-node/firstAPI/src/controller/UserController.ts
+++ aplicacoes-node/firstAPI/src/controller/UserController.ts
@@ -5,6 +5,16 @@ import { prisma } from "../database/prisma"
 export const createUser = async (req: Request, res: Response) => {
     const { name, email, password, accessName } = req.body
 
+    const isUserEmail = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    })
+
+    if (isUserEmail) {
+        return res.status(400).json({ message: "o email já existe, por favor digite outro email válido" })
+    }
+
     const user = await prisma.user.create({
         data: {
             name,
