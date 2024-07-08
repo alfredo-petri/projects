1a1dbe04f7c0af3a3f0114bf11488f59fb3f7332
alfredo-petri <alfredo_petri@hotmail.com>
2024-07-08 07:37:26 -0300

    unauthenticate routes

--- aplicacoes-node/firstAPI/src/routes.ts
+++ aplicacoes-node/firstAPI/src/routes.ts
@@ -8,18 +8,34 @@ import { authMiddlewhare } from "./middlewares/AuthMiddleware"
 
 export const router = Router()
 
+// router.post("/user", createUser)
+// router.get("/user", getUser)
+// router.get("/user/list", authMiddlewhare(["adm", "gerente"]), listUsers)
+// router.delete("/user/delete-all", authMiddlewhare(["adm"]), deleteAllUsers)
+
+// router.post("/access", authMiddlewhare(["adm", "gerente"]), createAccess)
+// router.get("/access/list", authMiddlewhare(["adm", "gerente"]), listAccess)
+
+// router.post("/store/:userId", authMiddlewhare(["adm"]), createStore)
+// router.get("/store/list", authMiddlewhare(["adm", "gerente"]), listStores)
+
+// router.post("/product/:storeId", authMiddlewhare(["adm", "gerente", "logistica"]), createProduct)
+// router.get("/product/list", authMiddlewhare(["adm", "gerente", "logistica", "vendedor"]), listProducts)
+
+// router.post("/sign-in", signIn)
+
 router.post("/user", createUser)
 router.get("/user", getUser)
-router.get("/user/list", authMiddlewhare(["adm", "gerente"]), listUsers)
-router.delete("/user/delete-all", authMiddlewhare(["adm"]), deleteAllUsers)
+router.get("/user/list", listUsers)
+router.delete("/user/delete-all", deleteAllUsers)
 
-router.post("/access", authMiddlewhare(["adm", "gerente"]), createAccess)
-router.get("/access/list", authMiddlewhare(["adm", "gerente"]), listAccess)
+router.post("/access", createAccess)
+router.get("/access/list", listAccess)
 
-router.post("/store/:userId", authMiddlewhare(["adm"]), createStore)
-router.get("/store/list", authMiddlewhare(["adm", "gerente"]), listStores)
+router.post("/store/:userId", createStore)
+router.get("/store/list", listStores)
 
-router.post("/product/:storeId", authMiddlewhare(["adm", "gerente", "logistica"]), createProduct)
-router.get("/product/list", authMiddlewhare(["adm", "gerente", "logistica", "vendedor"]), listProducts)
+router.post("/product/:storeId", createProduct)
+router.get("/product/list", listProducts)
 
 router.post("/sign-in", signIn)
