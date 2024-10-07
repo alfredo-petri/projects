e28b883fb4574fb28ebda2fb48b333496c958942
alfredo-petri <alfredo_petri@hotmail.com>
2024-10-07 17:00:36 -0300

    authenticate

--- linux-scripts/wsl-scripts/dev_enviroment_instalation.sh
+++ linux-scripts/wsl-scripts/dev_enviroment_instalation.sh
@@ -29,6 +29,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 #[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 #[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 # Carrega nvm
+#teste
 
 source ~/.bashrc
 
